37892c9088b0c45c0c0531bbf6b2d76e09d2ae31
makeitokay <andrey.mu11@yandex.ru>
2020-05-13 22:00:43 +0500

    Add more professions

--- skill/utils/professions_game_utils.py
+++ skill/utils/professions_game_utils.py
@@ -9,5 +9,4 @@ async def get_random_professions():
     with open('skill/assets/professions.json', encoding='utf-8') as f:
         data = json.load(f)
 
-    #return QuestionSet(random.sample(data, k=settings.PROFESSIONS_GAME_QUESTIONS))
-    return QuestionSet(data[:5], save_objects=True)
+    return QuestionSet(random.sample(data, k=settings.PROFESSIONS_GAME_QUESTIONS), save_objects=True)
